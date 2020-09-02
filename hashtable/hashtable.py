6759cd22d67d04ec266b2af2ed06add4919f5bfe
damonbogich <damonbogich@gmail.com>
2020-09-02 15:56:15 -0400

    lookup table done

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -108,6 +108,7 @@ class HashTable:
         self.table[index].next = current_head
         self.count += 1
 
+        #change capacity if load factor > .7
         if self.get_load_factor() > .7:
             new_capacity = self.capacity * 2
 
@@ -136,6 +137,8 @@ class HashTable:
             if current.key == key:
                 current.value = None
                 self.count -= 1
+
+                #change capacity if load factor is < .2
                 if self.capacity > 8:
                     if self.get_load_factor() < .2:
                         new_capacity = self.capacity / 2
@@ -188,150 +191,6 @@ class HashTable:
                     self.put(current.key, current.value)
                     current = current.next
 
-
-
-
-
-
-
-##### Monday:::
-
-
-# class HashTableEntry:
-#     """
-#     Linked List hash table key/value pair
-#     """
-#     def __init__(self, key, value):
-#         self.key = key
-#         self.value = value
-#         self.next = None
-
-
-# # Hash table can't have fewer than this many slots
-# MIN_CAPACITY = 8
-
-
-# class HashTable:
-#     """
-#     A hash table that with `capacity` buckets
-#     that accepts string keys
-
-#     Implement this.
-#     """
-
-#     def __init__(self, capacity):
-#         self.capacity = MIN_CAPACITY
-        
-#         self.table = [None] * self.capacity
-
-
-#     def get_num_slots(self):
-#         """
-#         Return the length of the list you're using to hold the hash
-#         table data. (Not the number of items stored in the hash table,
-#         but the number of slots in the main list.)
-
-#         One of the tests relies on this.
-
-#         Implement this.
-#         """
-        
-#         return len(self.table)
-
-
-#     def get_load_factor(self):
-#         """
-#         Return the load factor for this hash table.
-
-#         Implement this.
-#         """
-#         # Your code here
-
-
-#     def fnv1(self, key):
-#         """
-#         FNV-1 Hash, 64-bit
-
-#         Implement this, and/or DJB2.
-#         """
-
-#         # Your code here
-
-
-#     def djb2(self, key):
-#         """
-#         DJB2 hash, 32-bit
-
-#         Implement this, and/or FNV-1.
-#         """
-        
-#         hash = 5381
-#         for c in key:
-#             hash = (hash * 33) + ord(c)
-#         return hash
-
-
-#     def hash_index(self, key):
-#         """
-#         Take an arbitrary key and return a valid integer index
-#         between within the storage capacity of the hash table.
-#         """
-#         #return self.fnv1(key) % self.capacity
-#         return self.djb2(key) % self.capacity
-
-#     def put(self, key, value):
-#         """
-#         Store the value with the given key.
-
-#         Hash collisions should be handled with Linked List Chaining.
-
-#         Implement this.
-#         """
-#         #find index where you put value:
-#         index = self.hash_index(key)
-
-#         #put value there
-#         self.table[index] = value
-
-
-
-#     def delete(self, key):
-#         """
-#         Remove the value stored with the given key.
-
-#         Print a warning if the key is not found.
-
-#         Implement this.
-#         """
-#         # Your code here
-#         #TODO: how could the key not be found? -- just for collisions?
-#         index = self.hash_index(key)
-
-#         self.table[index] = None
-
-
-#     def get(self, key):
-#         """
-#         Retrieve the value stored with the given key.
-
-#         Returns None if the key is not found.
-
-#         Implement this.
-#         """
-#         # Your code here
-#         index = self.hash_index(key)
-
-#         return self.table[index]
-
-
-#     def resize(self, new_capacity):
-#         """
-#         Changes the capacity of the hash table and
-#         rehashes all key/value pairs.
-
-#         Implement this.
-#         """
-#         # Your code here
 
 
 
--- hashtable/scratch.py
+++ hashtable/scratch.py
@@ -1,49 +1,97 @@
-table = [None] * 8
+### EX 1:
+
+# #fibonacci -- memoization (caching)
 
 
-def hashf(s):
-    b = s.encode()
+# cache = {}
 
-    total = 0
-    for i in b:
-        total += i
-
-    return total
-
-
-def get_index(key):
-    index_value = hashf(key)
-    index_value %= len(table)
-
-    return index_value
-
-def put(key, value):
-    #which slot in table is the value going?
-    index = get_index(key)
-    #store value at that spot
-    table[index] = value
-
-def get(key):
-    index = get_index(key)
-
-    return table[index]
-
-def delete(key):
-    index = get_index(key)
-
-    table[index] = None
+# def fib(n):
+#     if n <= 1:
+#         return n
     
+#     if n not in cache:
+#         cache[n] = fib(n - 1) + fib(n - 2)
+
+#     return cache[n]
+
+# for i in range(100):
+#     print(f"{i}: {fib(i)}")
 
 
 
-#GET with collisions:
-    #Get the index for the key
-    #Search the LL at that index for the entry for that key
-    #Return the value (or None if not found)
 
-#PUT WITH COLLISIONS:
-    #Get the index for the key
-    #Search the list for the key
-    #if it already exists, overwrite the value
-    #Else, insert the [key, value] at the head of the LL if not
 
+# ### EX 2:
+# #sort dictionary values:
+
+# d = {
+#     'foo': 12,
+#     'bar': 17,
+#     'qux': 2
+# }
+
+# #sorts by key, then value (if keys are the same):
+# i = list(d.items())
+
+# i.sort()
+
+# print(i)
+
+# # def comp(e):
+# #     return e[1]
+
+# # i.sort(key = comp)
+
+# #same as the function above
+# #comparing the 2nd tuple value 
+# i.sort(key=lambda e:e[1])
+# print(i)
+
+
+
+##### EX 3:
+
+#Loopkup table
+
+import math
+"""
+Build a lookup table for the inverse square root for numbers 1 - 1000.
+"""
+
+# inv_sqrt_table = {}
+
+# def inv_sqrt(n):
+#     return 1 / math.sqrt(n)
+
+# def build_lookup_table():
+#     for i in range(1, 1001):
+#         inv_sqrt_table[i] = inv_sqrt(i)
+
+# build_lookup_table()
+
+# print(inv_sqrt_table[10])
+# print(inv_sqrt_table[37])
+
+
+
+
+### EX 4:
+#count the number of occurences of a letter in a string
+
+def letter_count(s):
+    d = {}
+
+    for c in s:
+        if c.isspace():
+            continue
+        
+        c = c.upper()
+        
+        if c not in d:
+            d[c] = 1
+        else:
+            d[c] += 1
+        
+    return d
+
+print(letter_count('Hello there!'))
